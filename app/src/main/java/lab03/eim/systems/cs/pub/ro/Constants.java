0462c9207ba340c2c6726fb11cbe1986eea6ce2b
ioanrazvanstanciu <ioan_razvan.stanciu@stud.acs.upb.ro>
2023-03-27 00:59:14 +0300

    incarcare cerinte rezolvate

---
+++ app/src/main/java/lab03/eim/systems/cs/pub/ro/Constants.java
@@ -0,0 +1,22 @@
+package lab03.eim.systems.cs.pub.ro;
+
+import lab03.eim.systems.cs.pub.ro.R.*;
+
+public interface Constants {
+
+    final public static int buttonIds[] = {
+            id.number_0_button,
+            id.number_1_button,
+            id.number_2_button,
+            id.number_3_button,
+            id.number_4_button,
+            id.number_5_button,
+            id.number_6_button,
+            id.number_7_button,
+            id.number_8_button,
+            id.number_9_button,
+            id.star_button,
+            id.pound_button
+    };
+    final public static int PERMISSION_REQUEST_CALL_PHONE = 1;
+}
